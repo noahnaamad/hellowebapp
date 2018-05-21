ae2c00eaf409acb0bc2dcbd4972ba43d6e8faa63
noah <noahnaamad@gmail.com>
2018-05-21 14:29:03 -0700

    play with passing in variables

--- collection/views.py
+++ collection/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    number = 4
+    return render(request, 'index.html', {
+    'number': number,
+    })
