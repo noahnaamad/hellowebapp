946aab0aef1d2605dc1e976b991f42a64926c5bc
noah <noahnaamad@gmail.com>
2018-05-21 15:00:57 -0700

    added dynamic data

--- collection/admin.py
+++ collection/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+#import model
+from collection.models import Post
+
+#automated slug creation
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
 # Register your models here.
+admin.site.register(Post, PostAdmin)
--- collection/models.py
+++ collection/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
