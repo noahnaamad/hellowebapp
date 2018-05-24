6f516997985747ba65e5bb8cad339213d66f3ab5
noah <noahnaamad@gmail.com>
2018-05-23 19:46:03 -0700

    save changes

--- collection/views.py
+++ collection/views.py
@@ -2,11 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from collection.models import Post
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()
+    number = 4
     return render(request, 'index.html', {
-    'posts': posts,
+    'number': number,
     })
