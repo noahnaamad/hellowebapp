f12930b01b8fb8dbe26ec5783c232ed812859c85
noah <noahnaamad@gmail.com>
2018-05-26 15:35:55 -0700

    add post editing capability

--- collection/views.py
+++ collection/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from collection.models import Post
+from collection.forms import PostForm
 
 # Create your views here.
 def index(request):
@@ -15,4 +16,22 @@ def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_detail.html', {
         'post': post,
+    })
+
+def edit_post(request, slug):
+    post= Post.objects.get(slug=slug)
+    form_class = PostForm
+    #if the request is coming from a submitted form
+    if request.method == 'POST':
+        form = form_class(data=request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', slug=post.slug)
+    #if the request is for an empty form
+    else:
+        form = form_class(instance=post)
+
+    return render(request, 'posts/edit_post.html', {
+        'post': post,
+        'form': form,
     })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html"), name="contact"),
     url(r'^posts/(?P<slug>[-\w]+)/$', views.post_detail, name="post_detail"),
+    url(r'^posts/(?P<slug>[-\w]+)/edit/$', views.edit_post, name='edit_post'),
     url(r'^admin/', admin.site.urls),
 ]
