03831321f9c6c94746ad216c084fb2d8e0893f1d
noah <noahnaamad@gmail.com>
2018-05-31 15:17:12 -0700

    add registration functionality

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from collection import views
 from django.views.generic import TemplateView
@@ -24,5 +24,6 @@ urlpatterns = [
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html"), name="contact"),
     url(r'^posts/(?P<slug>[-\w]+)/$', views.post_detail, name="post_detail"),
     url(r'^posts/(?P<slug>[-\w]+)/edit/$', views.edit_post, name='edit_post'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
