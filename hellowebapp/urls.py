b47167f53cdc819d762d5ba0087c9bd3ba08a884
noah <noahnaamad@gmail.com>
2018-06-05 16:06:59 -0700

    fix password reset typos

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -32,8 +32,8 @@ urlpatterns = [
     url(r'^posts/(?P<slug>[-\w]+)/edit/$', views.edit_post, name='edit_post'),
     url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
     url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
-    url(r'^accounts/password/reset/(?P<uid64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
-    url(r'^accounts/password/done/$', password_reset_complete, {'template_name': 'registration/password_reseet_complete.html'}, name="password_reset_complete"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
+    url(r'^accounts/password/done/$', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name="password_reset_complete"),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
